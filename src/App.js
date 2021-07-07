74ae5b63d6f55b9146fa19c699796c52014734bc
akb.tech17 <akb.tech17@gmail.com>
2021-07-07 17:01:57 +0530

    created and wired pathvisualizer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react'
 import './App.css';
+import PathVisualizer from './PathVisualizer/PathVisualizer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <PathVisualizer />
     </div>
   );
 }
---
+++ src/PathVisualizer/PathVisualizer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class PathVisualizer extends Component {
+  // constructor() {}
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <h1>Hello this is my app!</h1>
+      </>
+    );
+  }
+}
